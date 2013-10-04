990dcf92d0f4e23d26dfe9e73eb3a60b0c826375
jbloom <jbloom@fhcrc.org>
2013-10-03 21:27:34 -0700

    Initial gh-pages documentation commit

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This package implements mutpath.
-
-This package is designed for building mutational paths through sequence
-space using mutation-mapped trees written by BEAST (using the MarkovJumps
-feature).
-
-Written by Jesse Bloom."""
